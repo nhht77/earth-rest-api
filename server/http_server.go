ba1af40f5f8ca6744e8adfb48824c2f5ef0b843d
Trung Nguyen <nhht77@gmail.com>
2022-04-06 02:40:09 +0300

    fix API typo

--- server/http_server.go
+++ server/http_server.go
@@ -19,11 +19,11 @@ func RunHTTP() error {
 	router.HandleFunc("/api/v1/continent/update", HandleUpdateContinent).Methods("PUT")
 	router.HandleFunc("/api/v1/continent/delete", HandleDeleteContinent).Methods("DELETE")
 
-	router.HandleFunc("/api/v1/countries", HandleContinents).Methods("GET")
-	router.HandleFunc("/api/v1/country", HandleContinent).Methods("GET")
-	router.HandleFunc("/api/v1/country/create", HandleCreateContinent).Methods("POST")
-	router.HandleFunc("/api/v1/country/update", HandleUpdateContinent).Methods("PUT")
-	router.HandleFunc("/api/v1/country/delete", HandleDeleteContinent).Methods("DELETE")
+	router.HandleFunc("/api/v1/countries", HandleCountries).Methods("GET")
+	router.HandleFunc("/api/v1/country", HandleCountry).Methods("GET")
+	router.HandleFunc("/api/v1/country/create", HandleCreateCountry).Methods("POST")
+	router.HandleFunc("/api/v1/country/update", HandleUpdateCountry).Methods("PUT")
+	router.HandleFunc("/api/v1/country/delete", HandleDeleteCountry).Methods("DELETE")
 
 	return ListenAndServe(":8080", router)
 }
